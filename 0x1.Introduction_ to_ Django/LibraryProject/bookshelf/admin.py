b054bbdfa0795d1b00044467ea059c80d12be9da
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-08-11 23:13:05 +0100

    Update admin.py

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,5 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year')
+
+admin.site.register(Book, BookAdmin)
