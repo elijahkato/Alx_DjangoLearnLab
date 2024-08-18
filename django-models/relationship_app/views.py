c75d0e347666a8d9d858a121bff24666db8a0079
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-08-19 00:26:41 +0100

    Add files via upload

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView  # Add this import for DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth.decorators import user_passes_test
+
+
 
 # Function-based view to list all books
 def list_books(request):
@@ -27,3 +29,24 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
