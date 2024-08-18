7b73b1ab7745f2feadcf72ffab2d8fdeba9b211f
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-08-19 00:59:59 +0100

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+   path('books/add/', views.add_book, name='add_book'),  # URL pattern for adding a book
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),  # URL pattern for editing a book
     path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
 
