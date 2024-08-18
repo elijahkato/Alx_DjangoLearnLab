79082046b09cd0cb86a4e9c62ca6510bca35e657
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-08-18 23:36:03 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book
-from .models import Library
+from django.views.generic.detail import DetailView  # Add this import for DetailView
+from .models import Book, Library
 
+# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
+# Class-based view to display library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
