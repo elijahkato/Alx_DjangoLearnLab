11efd759e4edd7ee1e3c37920fdde7ce017a7355
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-09-07 02:58:48 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -31,3 +31,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view article'),
+            ('can_create', 'Can create article'),
+            ('can_edit', 'Can edit article'),
+            ('can_delete', 'Can delete article'),
+        ]
+
+    def __str__(self):
+        return self.title
