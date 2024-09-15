b946be87f10e89c1299410be2cbd33e491a3822b
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-09-15 22:52:16 +0100

    Changes

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,25 @@
 # blog/forms.py
 from django import forms
 from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']  # Include the fields that users can edit
+
+
+# Custom registration form
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+# Profile update form
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,13 @@
-# blog/urls.py
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegisterView, ProfileUpdateView
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),  # Registration URL
+    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),  # Login URL
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),  # Logout URL
+    path('profile/', ProfileUpdateView.as_view(), name='profile'),
     path('', PostListView.as_view(), name='post-list'),  # URL for listing posts
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # URL for post details
     path('posts/new/', PostCreateView.as_view(), name='post-create'),  # URL for creating a post
