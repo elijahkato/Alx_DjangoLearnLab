1b7d2426347fbecb010521d49a4b495f6d0724e7
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-09-22 21:14:26 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework.views import APIView, status
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -18,3 +19,18 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class UserFeedView(APIView):
+    permission_classes = [IsAuthenticated]  # Enforce authentication
+
+    def get(self, request):
+        # Retrieve all users that the current user is following
+        following_users = request.user.following.all()
+        
+        # Get posts from these users, ordered by creation date in descending order
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        
+        # Serialize the posts
+        serializer = PostSerializer(posts, many=True)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
